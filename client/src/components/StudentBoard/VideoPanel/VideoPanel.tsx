480287e4f4b5bde8ec0c31ffeb6078e60ba7fd15
geekvinay <geekvinay002@gmail.com>
2024-05-11 16:54:24 +0530

    feat: fixed the videopanel issues

--- client/src/components/StudentBoard/VideoPanel/VideoPanel.tsx
+++ client/src/components/StudentBoard/VideoPanel/VideoPanel.tsx
@@ -1,14 +1,22 @@
 import { useEffect, useState } from "react";
 import { selectPeers, useHMSActions, useHMSStore, useVideo } from "@100mslive/react-sdk";
+import MdiVolumeOff from '@iconify-icons/mdi/volume-off';
+import MdiVolumeHigh from '@iconify-icons/mdi/volume-high';
+import MdiVideoOff from '@iconify-icons/mdi/video-off';
+import MdiVideoOn from '@iconify-icons/mdi/video';
+import MdiLoading from '@iconify-icons/mdi/loading';
+import { Icon } from "@iconify/react/dist/iconify.js";
+import placeholderImage from "../../../assets/user-placeholder.png";
 
 const VideoPanel = ({ isTeacher = true }) => {
   const hmsActions = useHMSActions();
   const sessionDetails = JSON.parse(localStorage.getItem("session-details") || "{}");
-  const { userName = "Student 12345", roomCode = "pdz-ieeq-jma" } = sessionDetails;
+  const { userName = "Student 12345", roomCode = "uoo-vets-nbt" } = sessionDetails;
   const peers = useHMSStore(selectPeers);
   const [isLoading, setIsLoading] = useState(true);
-
-  // Ref to store the video element
+  const [isAudioMuted, setIsAudioMuted] = useState(true);
+  const [isVideoMuted, setIsVideoMuted] = useState(false);
+  const [showControls, setShowControls] = useState(true);
 
   useEffect(() => {
     const handleJoin = async () => {
@@ -17,13 +25,10 @@ const VideoPanel = ({ isTeacher = true }) => {
         await hmsActions.join({
           userName: userName,
           authToken,
-          settings: {
-            isVideoMuted: !isTeacher,
-            isAudioMuted: !isTeacher,
-          },
           rememberDeviceSelection: true,
         });
         setIsLoading(false);
+        setShowControls(false);
       } catch (error) {
         console.error('Error joining room:', error);
       }
@@ -35,29 +40,70 @@ const VideoPanel = ({ isTeacher = true }) => {
   const teacherPeer = peers.find(peer => peer.roleName === 'teacher' && !peer.isLocal);
   const { videoRef } = useVideo({ trackId: teacherPeer?.videoTrack });
 
-  console.log('teacherPeer: ', teacherPeer);
+  const toggleAudio = () => {
+    setIsAudioMuted(prevState => !prevState);
+    console.log('isAudioMuted: ', isAudioMuted);
+  };
+
+  const toggleVideo = () => {
+    setIsVideoMuted(prevState => !prevState);
+    console.log('isAudioMuted: ', isVideoMuted);
+  };
 
   return (
-    <section className="VideoPanel relative">
-      {isLoading && (
-        <div className="absolute top-0 left-0 w-full h-full bg-white flex justify-center items-center">
-          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
-        </div>
-      )}
-      {teacherPeer && (
-        <div className="video-wrapper relative h-full w-full">
-          <div className="video-title absolute bottom-0 left-0 w-full p-2">
-            {teacherPeer.name}
+    <section className="VideoPanel relative ">
+      <div className="video-wrapper relative h-full w-full">
+        <div
+          className="local-video-container flex justify-start w-full h-[25vh] rounded-md relative"
+          style={{ backgroundImage: `url(${placeholderImage})`, backgroundSize: "cover" }}
+          onMouseEnter={() => setShowControls(true)}
+          onMouseLeave={() => setShowControls(false)}
+        >
+          <div className="video-overlay rounded-md w-full h-full absolute top-0 left-0" style={{ opacity: isVideoMuted ? 1 : 0 }}>
+            {isLoading && <Icon icon={MdiLoading} className="text-gray-500 text-4xl" />}
           </div>
           <video
             ref={videoRef}
-            className="video rounded-md"
-            muted
+            className={`local-video rounded-md h-full w-full object-cover ${isVideoMuted ? "opacity-0" : "opacity-100"} transition-all`}
             autoPlay
+            muted={isAudioMuted}
             playsInline
           />
+          {showControls && (
+            <div className="absolute top-0 left-0 flex h-full w-full justify-center items-center">
+              <div className="bg-black h-full w-full bg-opacity-50 rounded-md p-4 flex space-x-4 items-center justify-center hover:cursor-pointer">
+                {isLoading && (
+                  <div className="p-2 bg-white hover:shadow-sm rounded-xl cursor-pointer">
+                    <Icon icon={MdiLoading} className="animate-spin text-gray-900 text-4xl" />
+                  </div>
+                )}
+                {!isLoading && (
+                  <div className="p-2 bg-white hover:shadow-sm rounded-xl cursor-pointer" onClick={toggleAudio}>
+                    {isAudioMuted ? (
+                      <Icon icon={MdiVolumeOff} className="text-gray-500 text-2xl" />
+                    ) : (
+                      <Icon icon={MdiVolumeHigh} className="text-gray-500 text-2xl" />
+                    )}
+                  </div>
+                )}
+                {!isLoading && (
+                  <div className="p-2 bg-white hover:shadow-sm rounded-xl cursor-pointer" onClick={toggleVideo}>
+                    {isVideoMuted ? (
+                      <Icon icon={MdiVideoOff} className="text-gray-500 text-2xl" />
+                    ) : (
+                      <Icon icon={MdiVideoOn} className="text-gray-500 text-2xl" />
+                    )}
+                  </div>
+                )}
+              </div>
+            </div>
+          )}
         </div>
-      )}
+        <div className="video-title relative w-full p-2 font-medium z-10 flex">
+          {teacherPeer && <span className="text-md font-bold px-2 text-sky-700">TEACHER</span>}
+          {teacherPeer && teacherPeer.name}
+        </div>
+      </div>
     </section>
   );
 };
--- client/src/components/Teacherboard/VideoPanel/VideoPanel.tsx
+++ client/src/components/Teacherboard/VideoPanel/VideoPanel.tsx
@@ -5,15 +5,17 @@ import MdiMicrophone from '@iconify-icons/mdi/microphone';
 import MdiMicrophoneOff from '@iconify-icons/mdi/microphone-off';
 import MdiVideoOff from '@iconify-icons/mdi/video-off';
 import MdiVideoOn from '@iconify-icons/mdi/video';
+import MdiLoading from '@iconify-icons/mdi/loading';
+import placeholderImage from "../../../assets/user-placeholder.png";
 
 const VideoPanel = ({ isTeacher = true }) => {
   const hmsActions = useHMSActions();
   const { isLocalAudioEnabled, isLocalVideoEnabled, toggleAudio, toggleVideo } = useAVToggle();
   const sessionDetails = JSON.parse(localStorage.getItem("session-details") || "{}");
-  const { userName="Vinay Kishore", roomCode="pdz-ieeq-jma" } = sessionDetails;
+  const { userName = "Vinay Kishore", roomCode = "pdz-ieeq-jma" } = sessionDetails;
   const peers = useHMSStore(selectPeers);
   const [isLoading, setIsLoading] = useState(true);
-  const [showControls, setShowControls] = useState(false);
+  const [showControls, setShowControls] = useState(true);
 
   useEffect(() => {
     const handleJoin = async () => {
@@ -44,21 +46,17 @@ const VideoPanel = ({ isTeacher = true }) => {
   });
 
   return (
-    <section className="VideoPanel relative">
-      {isLoading && (
-        <div className="absolute top-0 left-0 w-full h-full bg-white flex justify-center items-center">
-          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
-        </div>
-      )}
+    <section className="VideoPanel relative w-full h-[25vh]">
       <div
-        className="local-video-container flex justify-start p-2 rounded-md relative"
+        className="local-video-container w-full h-full flex justify-start rounded-md relative"
+        style={{ backgroundImage: `url(${placeholderImage})`, backgroundSize: "cover" }}
         onMouseEnter={() => setShowControls(true)}
         onMouseLeave={() => setShowControls(false)}
       >
         {isTeacher && isLocalVideoEnabled && (
           <video
             ref={videoRef}
-            className="local-video rounded-md"
+            className="local-video w-full h-full object-cover rounded-md"
             autoPlay
             muted
             playsInline
@@ -67,20 +65,35 @@ const VideoPanel = ({ isTeacher = true }) => {
         {showControls && (
           <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
             <div className="bg-black h-full w-full bg-opacity-50 rounded-md p-4 flex space-x-4 items-center justify-center hover:cursor-pointer">
-              <div className="p-2 bg-white hover:shadow-sm rounded-xl cursor-pointer" onClick={toggleAudio}>
-                {isLocalAudioEnabled ? (
-                  <Icon icon={MdiMicrophone} className="text-gray-500 text-2xl" />
-                ) : (
-                  <Icon icon={MdiMicrophoneOff} className="text-gray-500 text-2xl" />
-                )}
-              </div>
-              <div className="p-2 bg-white hover:shadow-sm rounded-xl cursor-pointer" onClick={toggleVideo}>
-                {isLocalVideoEnabled ? (
-                  <Icon icon={MdiVideoOn} className="text-gray-500 text-2xl" />
-                ) : (
-                  <Icon icon={MdiVideoOff} className="text-gray-500 text-2xl" />
-                )}
-              </div>
+              {
+                isLoading && (
+                  <div className="p-2 bg-white hover:shadow-sm rounded-xl cursor-pointer" onClick={toggleAudio}>
+                    <Icon icon={MdiLoading} className="animate-spin text-gray-900 text-4xl" />
+                  </div>
+                )
+              }
+              {
+                !isLoading && (
+                  <div className="p-2 bg-white hover:shadow-sm rounded-xl cursor-pointer" onClick={toggleAudio}>
+                    {isLocalAudioEnabled ? (
+                      <Icon icon={MdiMicrophone} className="text-gray-500 text-2xl" />
+                    ) : (
+                      <Icon icon={MdiMicrophoneOff} className="text-gray-500 text-2xl" />
+                    )}
+                  </div>
+                )
+              }
+              {
+                !isLoading && (
+                  <div className="p-2 bg-white hover:shadow-sm rounded-xl cursor-pointer" onClick={toggleVideo}>
+                    {isLocalVideoEnabled ? (
+                      <Icon icon={MdiVideoOn} className="text-gray-500 text-2xl" />
+                    ) : (
+                      <Icon icon={MdiVideoOff} className="text-gray-500 text-2xl" />
+                    )}
+                  </div>
+                )
+              }
             </div>
           </div>
         )}
